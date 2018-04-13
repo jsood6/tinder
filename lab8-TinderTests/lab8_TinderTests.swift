b07f5d1039d4d743d97095bf4bd76052ed952388
Jigyasaa Sood <jsood@ucsd.edu>
2018-04-13 16:07:05 -0700

    initial commit

---
+++ lab8-TinderTests/lab8_TinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab8_TinderTests.swift
+//  lab8-TinderTests
+//
+//  Created by jsood on 4/13/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+import XCTest
+@testable import lab8_Tinder
+
+class lab8_TinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
