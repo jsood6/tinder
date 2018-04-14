b3fb38ed923e3c8041c407a049826901f560ecd2
Melissa Nguyen <nguyen.melissa23@gmail.com>
2018-04-13 20:30:44 -0700

    To-do: image disappears

--- lab8-Tinder/CardsViewController.swift
+++ lab8-Tinder/CardsViewController.swift
@@ -13,71 +13,95 @@ class CardsViewController: UIViewController {
     @IBOutlet weak var profileImageView: UIImageView!
     
     @IBOutlet var backgroundView: UIView!
-   
-    
+
     var cardInitialCenter: CGPoint!
+    var topOfView: Bool!
+    var location: CGPoint!
     //var cardNewPosition: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        cardInitialCenter = profileImageView.center
+        print("cardInitCenter:", cardInitialCenter)
+        
+//        let recognizer = UITapGestureRecognizer(target: self, action: #selector(tapImageView(_:)) )
+//        recognizer.numberOfTapsRequired = 1
+//        profileImageView.addGestureRecognizer(recognizer)
+        
         // Do any additional setup after loading the view.
     }
-
+    
+//    @objc func tapImageView(_ gesture: UITapGestureRecognizer){
+////        print("location:", self.location)
+//        if (gesture.state == .began){
+//            self.location = gesture.location(in: profileImageView)
+//            print("location:", self.location)
+//        }
+//    }
+    
     @IBAction func didPanImage(_ sender: UIPanGestureRecognizer) {
-        print("inside pan image function")
+    
+//        print("inside pan image function")
         let translation = sender.translation(in: view)
-        
+        let location = sender.location(in: profileImageView)
+//        print("sender", sender)
+//        print("translation", translation)
+//        print("location in didPanImage", location)
         //don't know how to check if user touched top or bottom half of the imageView(check step 2)
-        if(profileImageView.center.x > 180)
-        {
-            if(profileImageView.center.x > 230)
-            {
-                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(30 * M_PI / 180))
-                
-                profileImageView.animateWithDuration( 0.3, options: .CurveEaseOut)
-                               
-                                                      /*animations: { () -> Void in
-                                profileImageView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-                }, completion: nil)*/
-                
-                /*[UIView animateWithDuration:5
-                    delay:5
-                    options:UIViewAnimationOptionCurveEaseOut
-                    animations:^{
-                    CGRect frame = view.frame;
-                    frame.origin.y = self.view.frame.size.height-200;
-                    frame.origin.x = 0;
-                    view.frame = frame;
-                    }completion:^(BOOL finished) {}];*/
+        let midPoint = (profileImageView.frame.height / 2)
+        
+        if (location.y < midPoint){
+//            print("drag top of image", location.y)
+//            print("midPoint", midPoint)
+            if (translation.x < 0 ){
+                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(-15 * Double.pi / 180))
+                profileImageView.animationDuration = 0.5
+            } else {
+                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(15 * Double.pi / 180))
+                profileImageView.animationDuration = 0.5
             }
-            else{
-                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(30 * M_PI / 180))
+        } else {
+            if (translation.x < 0 ){
+                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(15 * Double.pi / 180))
+                profileImageView.animationDuration = 0.5
+            } else {
+                profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(-15 * Double.pi / 180))
+                profileImageView.animationDuration = 0.5
             }
-        }
-        else{
-            profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(-30 * M_PI / 180))
         }
         
         
+        
+    
         if (sender.state == .began){
             print("inside began")
             //let imageView = sender.view as! UIImageView
             profileImageView.isUserInteractionEnabled = true
-            
-            
             //profileImageView.center.y += backgroundView.frame.origin.y
-            
-            
             cardInitialCenter = profileImageView.center
-            
-            
         } else if (sender.state == .changed){
             profileImageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y + translation.y)
         } else if (sender.state == .ended){
             cardInitialCenter = profileImageView.center
         }
     }
+    
+    func imgAppearance(_ translation: CGPoint){
+        if (translation.x > 50){
+            profileImageView.isHidden = true
+            self.profileImageView.center = CGPoint(x: self.cardInitialCenter.x , y: self.cardInitialCenter.y)
+            profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi / 180))
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                self.profileImageView.isHidden = false
+            }
+            
+        } else {
+            self.profileImageView.center = CGPoint(x: cardInitialCenter.x , y: cardInitialCenter.y)
+            profileImageView.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi / 180))
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
